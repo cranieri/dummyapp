1558a7e69ce0f20338f54d655d9f8e45ad496a59
Cosimo Ranieri <cosimo.ranieri@currencycloud.com>
2016-12-30 12:23:16 +0000

    fix test

--- spec/controllers/dummy_controller_spec.rb
+++ spec/controllers/dummy_controller_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe DummyController, type: :controller do
   describe 'GET#index' do
     it 'assigns a message to instnce variable' do
       get :index
-      expect(assigns(:greeting)).to eq "His everybody"
+      expect(assigns(:greeting)).to eq "Hi everybody"
     end
   end
 end
