43fff18457ee333933d297636ac1bd01fb818f90
Cosimo Ranieri <cosimo.ranieri@currencycloud.com>
2017-01-09 17:24:00 +0000

    new greeting

--- spec/controllers/dummy_controller_spec.rb
+++ spec/controllers/dummy_controller_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe DummyController, type: :controller do
   describe 'GET#index' do
-    it 'assigns a message to instnce variable' do
+    it 'assigns a message to instance variable' do
       get :index
       expect(assigns(:greeting)).to eq "Hi everybody"
     end
