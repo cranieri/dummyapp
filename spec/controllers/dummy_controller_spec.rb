1655eefba39aca6156cfeffd057a069534847452
Cosimo Ranieri <cosimo.ranieri@currencycloud.com>
2017-05-23 11:54:15 +0100

    adding issues for codeclimate

--- spec/controllers/dummy_controller_spec.rb
+++ spec/controllers/dummy_controller_spec.rb
@@ -3,9 +3,9 @@ require 'rails_helper'
 RSpec.describe DummyController, type: :controller do
   describe 'GET#index' do
     it 'assigns a message to instance variable' do
-      # get :index
-      # expect(assigns(:greeting)).to eq "Hi everybody"
-      expect(1).to eq 1
+      get :index
+      expect(assigns(:greeting)).to eq "Hi everybody"
+      # expect(1).to eq 1
     end
   end
 end
